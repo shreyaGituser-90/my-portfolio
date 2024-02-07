f22622b4237838853d0f7bf559e1aac98b170980
Shreya <sdniv46@gmail.com>
2024-02-08 00:40:09 +0530

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,48 @@
       document.getElementById(button.dataset.id).classList.add("active");
     });
   });
+  // document.body.classList.add("sbmt-txt");
+  // var element = document.querySelectorAll(".main-btn");
+  // element.classList.add("sbmt-txt");
   document.querySelector(".theme-btn").addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
   });
+
+  document.getElementById("sbmt").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting
+
+    // Send the email
+    emailjs
+      .sendForm(
+        "service_b9oozlp",
+        "template_oa28jfv",
+        form.current,
+        "CBWhojjersGoi5TSt"
+      )
+      .then(
+        function (response) {
+          console.log("Success!", response.status, response.text);
+        },
+        function (error) {
+          console.log("Failed...", error);
+        }
+      );
+  });
 })();
+function handleButtonClick() {
+  emailjs
+    .sendForm(
+      "service_b9oozlp",
+      "template_oa28jfv",
+      form.current,
+      "CBWhojjersGoi5TSt"
+    )
+    .then(
+      function (response) {
+        console.log("Success!", response.status, response.text);
+      },
+      function (error) {
+        console.log("Failed...", error);
+      }
+    );
+}
